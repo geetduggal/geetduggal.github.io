67c00e9ac2bf98fb5fc085f9ca65d97d23e3e2bd
Geet Duggal <geetduggal@daryas-mbp.lan>
2024-07-24 16:08:01 -0700

    Adding older-school research papers

--- render_academic.py
+++ render_academic.py
@@ -6,12 +6,12 @@ print("""
 """)
 for line in sys.stdin:
     fields = line.rstrip().split("\t")
-    authors, title, publication, volume, number, pages, year, publisher, _ = fields
+    authors, title, publication, volume, number, pages, year, publisher, pdf, url = fields
     print("<tr>")
-    print(f"<td>{line}</td>")
+    print(f"<td>{authors} <a href='{url}'>{title}</a> {publication} {volume} {number} {pages} {year} {publisher} (<a href='lib/{pdf}'>pdf</a>)</td>")
     print("</tr>")
 print("</table>")
-print("""<p>Profile on
+print("""<p>* indicates equal contribution. Profile on
 <a href='https://scholar.google.com/citations?user=1APlUC8AAAAJ&hl=en'>Google Scholar</a>.
 </p>
 """)
