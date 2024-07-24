3a95c26476c36a52a18612b2234bfaabfde3c608
Geet Duggal <geetduggal@daryas-mbp.lan>
2024-07-23 23:12:30 -0700

    Minor mod

--- render_geets.py
+++ render_geets.py
@@ -1,6 +1,6 @@
 import sys
 
-print("<table width=80%>")
+print("<table>")
 for line in sys.stdin:
 	date, geet = line.rstrip().split("\t")
 	print(f"<tr><td nowrap>{date}</td><td>{geet}</td></tr>")
