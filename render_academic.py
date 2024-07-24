682291b75a0e7fe8523790b9b6ebccaf5a34bae3
Geet Duggal <geetduggal@daryas-MacBook-Pro.local>
2024-07-24 10:24:03 -0700

    Minor mod

---
+++ render_academic.py
@@ -0,0 +1,18 @@
+import sys
+
+print("""
+<h2>Academic</h2>
+<table>
+""")
+for line in sys.stdin:
+    fields = line.rstrip().split("\t")
+    # authors, title, publication, volume, number, pages, year, publisher = fields
+    print("<tr>")
+    print(f"<td>{line.replace("\t", " ")}</td>")
+    print("</tr>")
+print("</table>")
+print("""<p>Profile on
+<a href='https://scholar.google.com/citations?user=1APlUC8AAAAJ&hl=en'>Google Scholar</a>.
+</p>
+""")
+
