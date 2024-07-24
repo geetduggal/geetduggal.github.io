67050dc60af9593dee9b6e70610594885bb101a2
Geet Duggal <geetduggal@daryas-mbp.lan>
2024-07-23 23:48:11 -0700

    Minor mod

--- render_geets.py
+++ render_geets.py
@@ -5,9 +5,9 @@ for line in sys.stdin:
 	date, geet = line.rstrip().split("\t")
 	print(f"<tr><td nowrap>{date}</td><td>{geet}</td></tr>")
 print("</table>")
-print("""<p align=right>
+print("""<p>
 See all Geets with ability to permalink on
-<a href='https://github.com/geetduggal/geetduggal.github.io/blob/master/geets.tsv'>Github<a/>
+<a href='https://github.com/geetduggal/geetduggal.github.io/blob/master/geets.tsv'>Github</a>
 </p>
 """)
 
