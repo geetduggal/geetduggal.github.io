21cb0204f9d86b1d21a0b900c01150936dadfbff
Geet Duggal <geetduggal@daryas-mbp.lan>
2024-07-24 18:00:51 -0700

    Adding articles

--- render_geets.py
+++ render_geets.py
@@ -2,8 +2,8 @@ import sys
 
 print("<table>")
 for line in sys.stdin:
-	date, geet = line.rstrip().split("\t")
-	print(f"<tr><td nowrap>{date}</td><td>{geet}</td></tr>")
+    date, geet = line.rstrip().split("\t")
+    print(f"<tr><td nowrap>{date}</td><td>{geet}</td></tr>")
 print("</table>")
 print("""<p>
 See all Geets with ability to permalink on
