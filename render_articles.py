4a7f36454470ec6975bffb9d70e23da01689f2ed
Geet Duggal <geetduggal@daryas-mbp.lan>
2024-07-24 22:54:53 -0700

    Adding articles

--- render_articles.py
+++ render_articles.py
@@ -3,5 +3,5 @@ import sys
 print("<h2>Articles</h2><table>")
 for line in sys.stdin:
     date, title, pdf, url  = line.rstrip().split("\t")
-    print(f"<tr><td nowrap>{date}</td><td><a href='{url}'>{title}</a></td></tr>")
+    print(f"<tr><td nowrap width=50px>{date}</td><td><a href='{url}'>{title}</a></td></tr>")
 print("</table>")
