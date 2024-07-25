941a54549b2d4c9c42f6166884b27b937626f405
Geet Duggal <geetduggal@daryas-mbp.lan>
2024-07-24 22:53:54 -0700

    Adding articles

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 all: index.html
 
-index.html: header.html footer.html render_geets.py geets.tsv render_academic.py articles.tsv render_articles.py
+index.html: header.html footer.html render_geets.py geets.tsv render_academic.py articles.tsv render_articles.py style.css
 	cat header.html > index.html
 	tail -8 geets.tsv | tac | python3 render_geets.py >> index.html
 	cat articles.tsv | tac | python3 render_articles.py >> index.html
--- render_geets.py
+++ render_geets.py
@@ -1,10 +1,40 @@
 import sys
 
 print("<table>")
-for line in sys.stdin:
+for i,line in enumerate(sys.stdin):
     date, geet = line.rstrip().split("\t")
-    print(f"<tr><td nowrap>{date}</td><td>{geet}</td></tr>")
+    if i == 0:
+        latest_geet = geet
+        print(f"<tr><td nowrap width=50px>{date}</td><td nowrap><div id='typing' class='typing-animation'></div></td></tr>")
+    else:
+        print(f"<tr><td nowrap width=50px>{date}</td><td>{geet}</td></tr>")
 print("</table>")
+
+print(f"""
+<script>
+  const text = "{latest_geet}";
+  let index = 0;
+""")
+print("""
+  function type() {
+      if (index < text.length) {
+          document.getElementById('typing').textContent += text.charAt(index);
+          index++;
+          setTimeout(type, 50);
+      } else {
+        setTimeout(() => {
+          document.getElementById('typing').innerHTML = text;
+          document.getElementById('typing').classList.remove('typing-animation');
+          document.getElementById('typing').style.borderRight = 'none';
+
+        }, 1000);
+      }
+  }
+
+  type();
+</script>
+""")
+
 print("""<p>
 See all Geets with ability to permalink on
 <a href='https://github.com/geetduggal/geetduggal.github.io/blob/master/geets.tsv'>Github</a>.
