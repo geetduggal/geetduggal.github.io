30390a1bc37224f691a76fc5075376b02cc4b290
Geet Duggal <geetduggal@daryas-mbp.lan>
2024-07-27 02:03:27 -0700

    testing geeter github oauth

--- netlify/functions/exchange_token.js
+++ netlify/functions/exchange_token.js
@@ -1,27 +1,54 @@
 const fetch = require('node-fetch');
 
+const headers = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Headers': 'Content-Type',
+    'Access-Control-Allow-Methods': 'POST, OPTIONS'
+};
+
 exports.handler = async (event) => {
+    if (event.httpMethod === 'OPTIONS') {
+        // Handle CORS preflight request
+        return {
+            statusCode: 200,
+            headers: headers,
+            body: ''
+        };
+    }
+
     const { code } = JSON.parse(event.body);
     const clientId = process.env.GITHUB_CLIENT_ID;
     const clientSecret = process.env.GITHUB_CLIENT_SECRET;
 
-    const response = await fetch('https://github.com/login/oauth/access_token', {
-        method: 'POST',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            client_id: clientId,
-            client_secret: clientSecret,
-            code: code
-        })
-    });
+    try {
+        const response = await fetch('https://github.com/login/oauth/access_token', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                client_id: clientId,
+                client_secret: clientSecret,
+                code: code
+            })
+        });
 
-    const data = await response.json();
-    return {
-        statusCode: 200,
-        body: JSON.stringify({ access_token: data.access_token })
-    };
+        const data = await response.json();
+        console.log('GitHub response data:', data);
+
+        return {
+            statusCode: 200,
+            headers: headers,
+            body: JSON.stringify({ access_token: data.access_token })
+        };
+    } catch (error) {
+        console.error('Error fetching access token:', error);
+        return {
+            statusCode: 500,
+            headers: headers,
+            body: JSON.stringify({ error: 'Internal Server Error' })
+        };
+    }
 };
 
