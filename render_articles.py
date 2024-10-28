ecc4e09a9fbe8e23ae8f504e82e9f9a40d14ec10
Geet Duggal <geetduggal@gmail.com>
2024-10-28 00:35:41 -0700

    minor changes

--- render_articles.py
+++ render_articles.py
@@ -4,6 +4,7 @@ import csv
 print("<h2>Articles</h2><table>")
 csv_reader = csv.reader(sys.stdin)
 for line in csv_reader:
+    print(line)
     date, title, pdf, url  = line
     print(f"<tr><td nowrap width=50px>{date}</td><td><a href='{url}'>{title}</a></td></tr>")
 print("</table>")
